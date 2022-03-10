15922c1eaafcc2fe090f555701ca6d1272b00d35
johntgetty <johntgettydesigns@gmail.com>
2022-03-10 11:47:46 -0500

    update

---
+++ starterTemplate.php
@@ -0,0 +1,7 @@
+<?php
+include_once 'header.php'
+?>
+<!-- CONTENT (directly below Botaris logo within the body tag including navbar) -->
+
+<div class="content">This is where stuff goes to start a new page</div>
+<?php include_once 'footer.php' ?>
